59aa2af2d1eda652788e9313a796fd3ec5cecddc
indrayansyah <62071367+indrayansyah@users.noreply.github.com>
2021-06-05 19:50:33 +0700

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,3 @@
+var data = [['Keren'],['D','W','B','T','A','S','U','D','M','O','Y','T','I','D'],['Sekali']]
+
+console.log(data[1][0], data[1][4],data[1][8], data[1][1], data[1][4], data[1][10], data[1][5], data[1][7], data[1][9], data[1][11], data[1][12], data[1][13], data[0][0].toUpperCase(), data[2][0].toUpperCase())
